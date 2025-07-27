871ee9b1dbd66a5fbe3b43dd61d7ad07853dc4c6
Jun333d <jun3d90@gmail.com>
2025-07-27 18:05:47 +0530

    Update upload-image.js

--- netlify/functions/upload-image.js
+++ netlify/functions/upload-image.js
@@ -3,24 +3,24 @@ import ImageKit from "imagekit";
 const imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-  urlEndpoint: "https://ik.imagekit.io/m1x4g4l6m"
+  urlEndpoint: "https://ik.imagekit.io/m1x4g4l6m/"
 });
 
-export const handler = async ({ body }) => {
+export const handler = async (event) => {
   try {
-    const { fileName, fileData } = JSON.parse(body);
-    // fileData should be a base64 string without data mime prefix
-    const uploadResult = await imagekit.upload({
+    const { fileName, fileData } = JSON.parse(event.body);
+
+    const result = await imagekit.upload({
       file: fileData,
       fileName,
       folder: "/images"
     });
+
     return {
       statusCode: 200,
-      body: JSON.stringify(uploadResult)
+      body: JSON.stringify(result)
     };
   } catch (err) {
-    console.error(err);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: err.message })
